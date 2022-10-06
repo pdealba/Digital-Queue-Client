45099d56cc5e3d640f4253848dd65365e882e823
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-10-06 16:19:10 -0300

    Added Header component which will redirect to either, the Timer or Chat component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
+import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Client from "./components/Client";
+import Chat from "./components/Chat/Chat";
+import Header from "./components/Layout/Header";
+import Timer from "./components/Timer/Timer";
 
 function App() {
-  return <Client />;
+  return (
+    <>
+      <Header />
+      <Routes>
+        <Route path="/timer" element={<Timer />} />
+        <Route path="/chat" element={<Chat />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
