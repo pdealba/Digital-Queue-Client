8638f39d66a6a63b2d3c33da9bec32e546159c6c
pedroDeAlba123 <pedrodealbaalegre@gmail.com>
2022-12-14 12:15:24 -0300

    added forum to let user fill in the information of the client

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
-import Chat from "./components/Chat/Chat";
 import Layout from "./components/hoc/Layout/Layout";
+import SignInForum from "./components/admin/SignInForum";
 import Timer from "./components/Timer/Timer";
+import Chat from "./components/Chat/Chat";
 
 function App() {
   return (
     <Layout>
       <Routes>
+        <Route path="/admin/new-user" element={<SignInForum />} />
         <Route path="/timer" element={<Timer />} />
         <Route path="/chat" element={<Chat />} />
       </Routes>
